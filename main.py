afbde70dba2d0962624750da67c146b687f523bb
Asier García Espido <33459159+agespido@users.noreply.github.com>
2020-11-10 19:26:18 +0100

    Code improvements
    Credentials are now read from a file, not in the main file anymore.
    
    Main code has been divided into different functions.

--- main.py
+++ main.py
@@ -1,20 +1,55 @@
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import geckodriver_autoinstaller
+import yaml
 
-geckodriver_autoinstaller.install()
+# For this script to work properly, the two-step verification must be off.
+# I can't take your phone and read your SMS code, sorry :)
 
-LOGIN_URL = "https://www.linkedin.com"
+def logIn(driver):
 
-# if chromedriver is not in your path, you’ll need to add it here
-cap = DesiredCapabilities().FIREFOX
-cap["marionette"] = True
+	# Get LinkedIn credentials
+	keys = yaml.load(open('.credentials.yml'), Loader=yaml.FullLoader)
 
-driver = webdriver.Firefox(capabilities=cap)
+	# Log in page
+	LOGIN_URL = "https://www.linkedin.com"
 
-# Log in screen
-driver.get (LOGIN_URL)
-driver.find_element_by_id('session_key').send_keys('user@mail.com')
-driver.find_element_by_id('session_password').send_keys('password')
-driver.find_element_by_class_name('sign-in-form__submit-button').click()
-#driver.quit()
+	driver.get (LOGIN_URL)
+	driver.find_element_by_id('session_key').send_keys(keys['LinkedInKeys']['username'])
+	driver.find_element_by_id('session_password').send_keys(keys['LinkedInKeys']['password'])
+	driver.find_element_by_class_name('sign-in-form__submit-button').click()
+
+	# If two-step verification is on, a timeout can be set here and 
+	# wait until the verification code is introduce manually.
+
+def getPersonalData():
+	pass
+
+def getSummary():
+	pass
+
+def getExperience():
+	pass
+
+def getEducation():
+	pass
+
+def main():
+	geckodriver_autoinstaller.install()
+
+	PROFILE_URL = "https://www.linkedin.com/in/"
+
+	# if chromedriver is not in your path, you’ll need to add it here
+	cap = DesiredCapabilities().FIREFOX
+	cap["marionette"] = True
+
+	driver = webdriver.Firefox(capabilities=cap)
+	logIn(driver)
+
+	# Profile page
+	driver.get (PROFILE_URL)
+	
+	#driver.quit()
+
+if __name__ == "__main__":
+	main()
